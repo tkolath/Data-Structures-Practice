74a980ed60e8a317c270b5dd8427cafa2fbeda99
Thomas Kolath <kolaththomas@yahoo.com>
2024-12-23 09:14:12 -0500

    updated

--- playground/practice.cpp
+++ playground/practice.cpp
@@ -1,26 +1,60 @@
-//string reversal with stack
+//reverse linked list with stack
 #include <iostream>
 #include <stack>
 
-void reverse(char *c) {
-    std::stack<char> s; 
-    int n = strlen(c);
+struct Node {
+    int data; 
+    Node* next;
+};
+Node* head; 
 
-    for (size_t i{}; i<n; i++) {
-        s.push(c[i]);
+void insert(int data) {
+    Node* temp1 = new Node(); 
+    temp1 -> data = data; 
+    temp1 -> next = nullptr; 
+    if (head == nullptr) {
+        temp1 -> next = head; 
+        head = temp1;
+        return; 
     }
-
-    for (size_t i{}; i<n; i++) {
-        c[i] = s.top(); 
-        s.pop();
+    Node* temp2 = head; 
+    while (temp2 -> next != nullptr) {
+        temp2 = temp2 -> next; 
     }
+    temp2 -> next = temp1; 
+}
+void print() {
+    Node* temp1 = head; 
+    while (temp1 != nullptr) {
+        std::cout << temp1 -> data << " ";
+        temp1 = temp1 -> next; 
+    }
+    std::cout << std::endl; 
+}
+void reverse() { 
+    if (head == nullptr) return; 
+    Node* temp1 = head; 
+    std::stack<Node*> s; 
+    while (temp1 != nullptr) {
+        s.push(temp1);
+        temp1 = temp1 -> next; 
+    }
+    temp1 = s.top(); 
+    head = temp1; 
+    s.pop(); 
+    while (!s.empty()) {
+        temp1 -> next = s.top(); 
+        temp1 = temp1 -> next; 
+        s.pop(); 
+    }
+    temp1 -> next = nullptr;
 }
 
 int main() {
-    char c[51];
-    std::cout << "enter string" << std::endl; 
-    std::cin >> c; 
-    reverse(c);
-    printf("result = %s \n", c);
-    return 0; 
+    insert(5);
+    insert(6);
+    insert(8);
+    print(); 
+    reverse(); 
+    print(); 
 }

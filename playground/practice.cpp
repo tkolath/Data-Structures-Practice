04cdf4ddd40ab5200bffce5447428163a7379b9a
Thomas Kolath <kolaththomas@yahoo.com>
2024-12-27 07:49:45 -0500

    postfix

--- playground/practice.cpp
+++ playground/practice.cpp
@@ -1,33 +1,55 @@
-//stack_balanced_parentheses
 #include <iostream>
 #include <stack>
 #include <string>
 
-bool are_pair (char opening, char closing) {
-    if (opening == '(' && closing == ')' || opening == '{' && closing == '}'|| opening == '[' && closing == ']') return true;
+bool is_operator(char c) {
+    if (c == '+' || c == '-' || c == '*' || c == '/') return true;
+    return false; 
+}
+
+bool is_numeric_digit(char c) {
+    if (c >= '0' && c <= '9') return true; 
     return false;
 }
 
-bool has_balanced_parenthesis(std::string exp) {
-    std::stack <char> s;
-    for (size_t i{}; i < exp.length(); i++) {
-        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[') s.push(exp[i]);
-        else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']') {
-            if (s.empty() || !are_pair(s.top(), exp[i])) return false;
-            else s.pop();
+int perform_operation(char operation, int operand1, int operand2) {
+    if (operation == '+') return operand1 + operand2;
+    else if (operation == '-') return operand1 - operand2; 
+    else if (operation == '*') return operand1 * operand2; 
+    else if (operation == '/') return operand1 / operand2; 
+
+    else std::cout << "Unexpected Error\n";
+    return -1; 
+}
+
+int evaluate_postfix(std::string expression){
+    std::stack <int> s;
+    for (size_t i{};i < expression.length(); i++){
+        if (expression[i] == ' ' || expression[i] == ',') continue;
+        else if (is_operator(expression[i])) {
+            int operand2 = s.top(); s.pop();
+            int operand1 = s.top(); s.pop();
+            int result = (perform_operation(expression[i], operand1, operand2));
+            s.push(result);
         }
-    }
-    return s.empty() ? true:false;
+        else if (is_numeric_digit(expression[i])) {
+            int operand = 0; 
+            while (i < expression.length() && is_numeric_digit(expression[i])) {
+                operand = (operand*10) + (expression[i]-'0');
+                i++;
+            }
+            i--;
+            s.push(operand);
+        }
+    } 
+    return s.top();
 }
 
 int main() {
-    std::string expression; 
-    std::cout << "Enter the Expression";
-    std::cin >> expression;
-    if(has_balanced_parenthesis(expression))  {
-        std::cout << "Balanced\n";
-    }
-    else {
-        std::cout << "Not Balanced\n" << std::endl;
-    }
+    std::string expression;
+    std::cout << "Enter expression: " << std::endl; 
+    std::getline(std::cin, expression);
+    int result = evaluate_postfix(expression);
+    std::cout << "Output : " << result << "\n";
+
 }

41b9bf2575a6116ca1aedba2c0e0a68d511d0243
tkolath <kolaththomas@yahoo.com>
2025-02-03 10:11:28 -0500

    update

--- playground/16.queue_array.cpp
+++ playground/16.queue_array.cpp
@@ -2,93 +2,78 @@
 #include <iostream>
 #define MAX_SIZE 101
 
-class Queue
-{
+class Queue {
 private:
-	int A[MAX_SIZE];
-	int front, rear; 
+    int A[MAX_SIZE];
+    int front, rear; 
 public:
-	Queue()
-	{
-		front = -1; 
-		rear = -1;
-	}
+    Queue() {
+        front = -1; 
+        rear = -1;
+    }
 
-	bool IsEmpty()
-	{
-		return (front == -1 && rear == -1); 
-	}
+    bool IsEmpty() {
+        return (front == -1 && rear == -1);
+    }
 
-	bool IsFull()
-	{
-		return (rear+1)%MAX_SIZE == front ? true : false;
-	}
+    bool IsFull() {
+        return (rear + 1) % MAX_SIZE == front ? true : false; 
+    }
 
-	void Enqueue(int x)
-	{
-		std::cout<<"Enqueuing "<<x<<" \n";
-		if(IsFull())
-		{
-			std::cout<<"Error: Queue is Full\n";
-			return;
-		}
-		if (IsEmpty())
-		{ 
-			front = rear = 0; 
-		}
-		else
-		{
-		    rear = (rear+1)%MAX_SIZE;
-		}
-		A[rear] = x;
-	}
+    void Enqueue(int x) {
+        std::cout << "Enqueuing " << x << " \n";
+        if (IsFull()) {
+            std::cout << "Error: Queue is full\n";
+            return; 
+        }
+        if (IsEmpty()) {
+            front = rear = 0;
+        }
+        else {
+            rear = (rear + 1) % MAX_SIZE;
+        }
+        A[rear] = x;
+    }
 
-	void Dequeue()
-	{
-		std::cout<<"Dequeuing \n";
-		if(IsEmpty())
-		{
-			std::cout<<"Error: Queue is Empty\n";
-			return;
-		}
-		else if(front == rear ) 
-		{
-			rear = front = -1;
-		}
-		else
-		{
-			front = (front+1)%MAX_SIZE;
-		}
-	}
+    void Dequeue(){
+        std::cout << "Dequeuing \n";
+        if (IsEmpty()) {
+            std::cout << "Error: Queue is empty";
+            return; 
+        }
+        else if (front == rear) {
+            front = rear = -1; 
+        }
+        else {
+            front = (front + 1) % MAX_SIZE;
+        }
+    }
 
-	int Front()
-	{
-		if(front == -1)
-		{
-			std::cout<<"Error: cannot return front from empty queue\n";
-			return -1; 
-		}
-		return A[front];
-	}
-	
-	void Print()
-	{
-		int count = (rear+MAX_SIZE-front)%MAX_SIZE + 1;
-		std::cout<<"Queue       : ";
-		for(int i = 0; i <count; i++)
-		{
-			int index = (front+i) % MAX_SIZE;
-			std::cout<<A[index]<<" ";
-		}
-		std::cout<<"\n\n";  
-	}
+    int Front(){
+        if (front == -1 ) {
+            std::cout << "Error: cannot return front from empty queue\n";
+            return -1; 
+        }
+        return A[front];
+    }
+
+    void Print() {
+        int count = (rear + MAX_SIZE - front) % MAX_SIZE + 1;
+        std::cout << "Queue     : ";
+        for (int i = 0; i < count; i++) {
+            int index = (front + i) % MAX_SIZE;
+            std::cout << A[index] << " ";
+        }
+        std::cout << "\n\n"; 
+    }
 };
-int main()
-{
-   Queue Q;
-   Q.Enqueue(2);  Q.Print();  
-   Q.Enqueue(4);  Q.Print();  
-   Q.Enqueue(6);  Q.Print();
-   Q.Dequeue();	  Q.Print();
-   Q.Enqueue(8);  Q.Print();
+
+int main() {
+    Queue Q;
+    Q.Enqueue(5); Q.Print();
+    Q.Enqueue(1); Q.Print();
+    Q.Enqueue(8); Q.Print();
+    Q.Dequeue(); Q.Print();
+    Q.Enqueue(3); Q.Print();
+
 }
